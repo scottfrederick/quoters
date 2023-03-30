e96ea3100e9964f25815efdca0a3f39ee6fec9f0
Scott Frederick <sfrederick@vmware.com>
2023-03-30 16:34:51 -0500

    Use Boot support for SSL bundles with RestTemplateBuilder

--- client-resttemplate/src/main/java/org/springframework/quotes/client/QuoteClientApplication.java
+++ client-resttemplate/src/main/java/org/springframework/quotes/client/QuoteClientApplication.java
@@ -1,14 +1,5 @@
 package org.springframework.quotes.client;
 
-import javax.net.ssl.SSLContext;
-
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,14 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.ssl.SslBundle;
-import org.springframework.boot.ssl.SslDetails;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -61,22 +50,9 @@ public class QuoteClientApplication {
 		private String sslBundle;
 
 		@Bean
-		public RestTemplateCustomizer restTemplateCustomizer(SslBundles sslBundles) {
-			return (restTemplate) -> {
-				SslBundle sslBundle = sslBundles.getBundle(this.sslBundle);
-				SSLContext sslContext = sslBundle.getSslContext();
-				SslDetails sslDetails = sslBundle.getDetails();
-
-				SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext,
-						sslDetails.getEnabledProtocols(),
-						sslDetails.getCiphers(),
-						new DefaultHostnameVerifier());
-				PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder
-						.create().setSSLSocketFactory(socketFactory).build();
-				CloseableHttpClient client = HttpClientBuilder.create().setConnectionManager(connectionManager).build();
-
-				restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(client));
-			};
+		public RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer, SslBundles sslBundles) {
+			SslBundle sslBundle = sslBundles.getBundle(this.sslBundle);
+			return configurer.configure(new RestTemplateBuilder()).setSslBundle(sslBundle);
 		}
 
 	}
